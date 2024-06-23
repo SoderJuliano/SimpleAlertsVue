7ae3a210b386367468469ad7e247b500c7df34d8
Juliano Soder <julianosoder1989@gmail.com>
2024-06-23 18:30:37 -0300

    fazendo configs pra exportar o css

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 import AlertComponent from './components/AlertComponent.vue';
 
-// Declare install function executed by Vue.use()
-export function install(Vue) {
-if (install.installed) return;
-install.installed = true;
-Vue.component('simple-alerts-vue', AlertComponent);
+export function install(app) {
+    if (install.installed) return;
+    install.installed = true;
+    app.component('simple-alerts-vue', AlertComponent);
 }
 
-// Create module definition for Vue.use()
 const plugin = {
-install,
+    install,
 };
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
-GlobalVue = window.Vue;
+    GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
-GlobalVue = global.Vue;
+    GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-GlobalVue.use(plugin);
+    GlobalVue.use(plugin);
 }
 
-// To allow use as module (npm/webpack/etc.) export component
 export default AlertComponent;
